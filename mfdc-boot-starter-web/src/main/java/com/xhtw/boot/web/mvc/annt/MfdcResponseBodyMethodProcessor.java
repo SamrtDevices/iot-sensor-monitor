6de446627b5002cd0b705d728c8f49dc64cb6944
Changxu Xing <xcx_xhgj@163.com>
2018-05-05 22:11:06 +0800

    update

---
+++ mfdc-boot-starter-web/src/main/java/com/xhtw/boot/web/mvc/annt/MfdcResponseBodyMethodProcessor.java
@@ -0,0 +1,50 @@
+/**
+ * 2018/5/5
+ */
+
+package com.xhtw.boot.web.mvc.annt;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.accept.ContentNegotiationManager;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import org.springframework.web.servlet.mvc.method.annotation.AbstractMessageConverterMethodProcessor;
+
+import java.util.List;
+
+/**
+ * @author (changxu.xcx)
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+public class MfdcResponseBodyMethodProcessor extends AbstractMessageConverterMethodProcessor {
+    protected MfdcResponseBodyMethodProcessor(List<HttpMessageConverter<?>> converters) {
+        super(converters);
+    }
+
+    protected MfdcResponseBodyMethodProcessor(List<HttpMessageConverter<?>> converters, ContentNegotiationManager contentNegotiationManager) {
+        super(converters, contentNegotiationManager);
+    }
+
+    protected MfdcResponseBodyMethodProcessor(List<HttpMessageConverter<?>> converters, ContentNegotiationManager manager, List<Object> requestResponseBodyAdvice) {
+        super(converters, manager, requestResponseBodyAdvice);
+    }
+
+    public boolean supportsParameter(MethodParameter methodParameter) {
+        return false;
+    }
+
+    public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
+        return null;
+    }
+
+    public boolean supportsReturnType(MethodParameter methodParameter) {
+        return false;
+    }
+
+    public void handleReturnValue(Object o, MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest) throws Exception {
+
+    }
+}
