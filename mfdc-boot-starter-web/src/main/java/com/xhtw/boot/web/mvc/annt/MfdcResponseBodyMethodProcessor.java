ab19fd5fa4587f1f38e92d0f5bb321c52cc5f342
Changxu Xing <xcx_xhgj@163.com>
2018-05-07 00:15:32 +0800

    update

--- mfdc-boot-starter-web/src/main/java/com/xhtw/boot/web/mvc/annt/MfdcResponseBodyMethodProcessor.java
+++ mfdc-boot-starter-web/src/main/java/com/xhtw/boot/web/mvc/annt/MfdcResponseBodyMethodProcessor.java
@@ -4,7 +4,9 @@
 
 package com.xhtw.boot.web.mvc.annt;
 
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Annotated;
 import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.bind.support.WebDataBinderFactory;
@@ -40,8 +42,9 @@ public class MfdcResponseBodyMethodProcessor extends AbstractMessageConverterMet
         return null;
     }
 
-    public boolean supportsReturnType(MethodParameter methodParameter) {
-        return false;
+    public boolean supportsReturnType(MethodParameter returnType) {
+        return (AnnotatedElementUtils.hasAnnotation(returnType.getContainingClass(), CosmoResponseBody.class)
+                || returnType.hasMethodAnnotation(CosmoResponseBody.class));
     }
 
     public void handleReturnValue(Object o, MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest) throws Exception {
