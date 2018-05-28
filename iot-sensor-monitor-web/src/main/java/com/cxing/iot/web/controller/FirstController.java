1000ff49cb13678bc8d7a3eb841b887600a590e8
Changxu Xing <xcx_xhgj@163.com>
2018-05-28 22:48:45 +0800

    去除mfdc moudel, 增加返回json格式化

--- iot-sensor-monitor-web/src/main/java/com/cxing/iot/web/controller/FirstController.java
+++ iot-sensor-monitor-web/src/main/java/com/cxing/iot/web/controller/FirstController.java
@@ -1,7 +1,11 @@
 package com.cxing.iot.web.controller;
 
+import com.cxing.iot.model.book.Book;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -10,5 +14,13 @@ public class FirstController {
     @RequestMapping("/index")
     public String index(){
         return "Hello Spring Boot";
+    }
+
+    @RequestMapping("/book")
+    public List<Book> getBook() {
+        List<Book> books = new LinkedList<>();
+        books.add(new Book("java 开发经典", 67, "电子出版社"));
+        books.add(new Book("python 开发经典", 57, "电子出版社"));
+        return books;
     }
 }
